eb083c15642f7ab27bf64cded7ff2aa4c018c2ca
Mihir Shah <mihirs@login2.stampede2.tacc.utexas.edu>
2022-04-08 09:16:38 -0500

    conf manager code

---
+++ lcache/rrdma/conf_client.h
@@ -0,0 +1,30 @@
+
+#ifndef _CONF_CLIENT_H_
+#define _CONF_CLIENT_H_
+
+#include <pthread.h>
+#include <stdint.h>
+#include <netinet/in.h>
+
+struct config {
+  int n;
+  struct in_addr* ips;
+  pthread_mutex_t mutex;
+};
+typedef struct config config_t;
+
+struct conf_cmd {
+  union {
+    uint8_t type;
+  } cmd;
+  pthread_mutex_t mutex;
+};
+typedef struct conf_cmd conf_cmd_t;
+
+void init_conf(config_t* conf);
+void init_cmd(conf_cmd_t* cmd);
+
+void start_appl_client(config_t* conf);
+void start_cache_client(conf_cmd_t* cmd);
+
+#endif
