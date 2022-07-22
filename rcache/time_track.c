e19988b28f17d2be07729675f215caa2288345cc
Mihir Shah <mihirs@login3.stampede2.tacc.utexas.edu>
2022-07-22 00:43:57 -0500

    save time_track

--- rcache/time_track.c
+++ rcache/time_track.c
@@ -1,4 +1,9 @@
 
+#include <stdlib.h>
+#include <assert.h>
+#include <stdint.h>
+#include <pthread.h>
+
 #define BUCKET_TS_INTV_USECS 20000
 #define LOG_SIZE 1024
 #define RING_LEN 32
@@ -8,7 +13,7 @@ enum alloc_entry_type {
   RETURN,
 };
 
-struct alloc_entry __attribute__((packed, aligned(4))) {
+struct __attribute__((packed, aligned(4))) alloc_entry {
   enum alloc_entry_type type : 8;
   uint32_t alloc_num : 24;
 };
@@ -29,8 +34,8 @@ union head_info {
 
 struct bucket_ring {
   struct alloc_log* buckets[RING_LEN];
-  struct pthread_spin_lock locks[RING_LEN];
-  union head_info head;
+  pthread_spinlock_t locks[RING_LEN];
+  uint64_t head;
 };
 
 static inline uint64_t to_usecs(struct timespec ts)
@@ -47,19 +52,19 @@ void bucket_ring_init(struct bucket_ring* ring, struct timespec ts)
     log->prev = NULL;
     ring->buckets[i] = log;
   }
-  ring->head_info.ts_usecs = to_usecs(ts);
-  ring->head_info.head_idx = 0;
-}
-
-static inline size_t get_ring_pos(struct bucket_ring* ring, uint64_t ts)
-{
+  
   union head_info info;
-  info.bits = __atomic_load_n(ring->head, __ATOMIC_SEQ_CST);
-  return (((ts - info.ts_usecs) / BUCKET_TS_INTV_USECS) + info.head_idx) & (RING_LEN-1);
+  info.ts_usecs = to_usecs(ts);
+  info.head_idx = 0;
+  ring->head = info.bits;
 }
 
 static inline void bucket_ring_append(struct bucket_ring* ring, uint64_t ts, uint32_t alloc_num, enum alloc_entry_type type)
 {
+  union head_info info;
+  info.bits = __atomic_load_n(&(ring->head), __ATOMIC_SEQ_CST);
+  size_t rpos = (((ts - info.ts_usecs) / BUCKET_TS_INTV_USECS) + info.head_idx) & (RING_LEN-1);
+
   pthread_spin_lock(&(ring->locks[rpos]));
   struct alloc_log* log = ring->buckets[rpos];
 
@@ -88,10 +93,61 @@ void bucket_ring_return(struct bucket_ring* ring, uint64_t ts, uint32_t alloc_nu
   bucket_ring_append(ring, ts, alloc_num, RETURN);
 }
 
+int cmp_alloc_entry(const void* a, const void* b)
+{
+  int va = (((const struct alloc_entry*) a)->alloc_num << 7)
+    | ((const struct alloc_entry*) a)->type;
+  int vb = (((const struct alloc_entry*) b)->alloc_num << 7)
+    | ((const struct alloc_entry*) b)->type;
+  return va - vb;
+}
+
 void bucket_ring_revoke(struct bucket_ring* ring, uint64_t ts_before)
 {
-  while (ring->ts_base
-  size_t rpos = (ts - __atomic_load_n(ring->ts_base, __ATOMIC_SEQ_CST)) / RING_LEN;
-  BUCKET_TS_INTV_MS;
-    size_t rpos = get_ring_pos(ring, ts_before);
+  while (1) {
+    union head_info info;
+    info.bits = __atomic_load_n(&(ring->head), __ATOMIC_SEQ_CST);
+
+    if (info.ts_usecs + BUCKET_TS_INTV_USECS < ts_before) {
+      pthread_spin_lock(&(ring->locks[info.head_idx]));
+      struct alloc_log* log = ring->buckets[info.head_idx];
+
+      while (log != NULL) {
+        if (log->pos > 0) {
+          qsort(log->blocks, log->pos, sizeof(struct alloc_entry), cmp_alloc_entry);
+
+          size_t lpos = 1;
+          assert(log->blocks[0].type == REGISTER);
+          while (lpos < LOG_SIZE) {
+            if (log->blocks[lpos].type == RETURN && log->blocks[lpos-1].type == REGISTER) {
+              lpos += 2;
+            } else if (log->blocks[lpos].type == REGISTER) {
+              // unmatched, we need to kill it ourself.
+              lpos += 1;
+            } else {
+              assert(0 && "Insufficient pattern match.");
+            }
+          }
+          log->pos = 0;
+        }
+        
+        struct alloc_log* prev = log->prev;
+        if (log != ring->buckets[info.head_idx]) {
+          free(log);
+        } else {
+          log->prev = NULL;
+        }
+        log = prev;
+      }
+      
+      pthread_spin_unlock(&(ring->locks[info.head_idx]));
+      info.ts_usecs += BUCKET_TS_INTV_USECS;
+      info.head_idx = (info.head_idx + 1) & (RING_LEN-1);
+      __atomic_store_n(&(ring->head), info.bits, __ATOMIC_SEQ_CST);
+
+    } else {
+      break;
+    }
+  }
+
 }
